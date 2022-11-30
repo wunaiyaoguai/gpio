9d58566c8ad045b15fa20b550656f518985b33f8
wunaiyaoguai <32297247+wunaiyaoguai@users.noreply.github.com>
2022-11-30 21:26:07 +0800

    Update io.go

--- io.go
+++ io.go
@@ -2,6 +2,7 @@ package gpio
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"time"
 )
@@ -14,26 +15,55 @@ type Pin struct {
 }
 
 // NewInput opens the given pin number for reading. The number provided should be the pin number known by the kernel
-func NewInput(p uint) Pin {
-	pin := Pin{
+func NewInput(p uint) (pin Pin, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+	pin = Pin{
 		Number: p,
 	}
 	exportGPIO(pin)
-	time.Sleep(10 * time.Millisecond)
+	// Wait while direction file will be accessible
+	// https://github.com/brian-armstrong/gpio/issues/6
+	for i := 0; i < 100; i++ {
+		f, err := os.OpenFile(fmt.Sprintf("/sys/class/gpio/gpio%d/direction", pin.Number), os.O_WRONLY, 0600)
+		if err == nil {
+			f.Close()
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 	pin.direction = inDirection
 	setDirection(pin, inDirection, 0)
 	pin = openPin(pin, false)
-	return pin
+	return pin, nil
 }
 
 // NewOutput opens the given pin number for writing. The number provided should be the pin number known by the kernel
 // NewOutput also needs to know whether the pin should be initialized high (true) or low (false)
-func NewOutput(p uint, initHigh bool) Pin {
-	pin := Pin{
+func NewOutput(p uint, initHigh bool) (pin Pin, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+
+	pin = Pin{
 		Number: p,
 	}
 	exportGPIO(pin)
-	time.Sleep(10 * time.Millisecond)
+	// Wait while direction file will be accessible
+	// https://github.com/brian-armstrong/gpio/issues/6
+	for i := 0; i < 100; i++ {
+		f, err := os.OpenFile(fmt.Sprintf("/sys/class/gpio/gpio%d/direction", pin.Number), os.O_WRONLY, 0600)
+		if err == nil {
+			f.Close()
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 	initVal := uint(0)
 	if initHigh {
 		initVal = uint(1)
@@ -41,21 +71,12 @@ func NewOutput(p uint, initHigh bool) Pin {
 	pin.direction = outDirection
 	setDirection(pin, outDirection, initVal)
 	pin = openPin(pin, true)
-	return pin
+	return pin, nil
 }
 
-// Close releases the resources related to Pin. This doen't unexport Pin, use Cleanup() instead
+// Close releases the resources related to Pin
 func (p Pin) Close() {
-	if p.f != nil {
-		p.f.Close()
-		p.f = nil
-	}
-}
-
-// Cleanup close Pin and unexport it
-func (p Pin) Cleanup() {
-	p.Close()
-	unexportGPIO(p)
+	p.f.Close()
 }
 
 // Read returns the value read at the pin as reported by the kernel. This should only be used for input pins
